5d78299aee474193456c398790618eb8118f6886
Fudy <104286054+Fudyy@users.noreply.github.com>
2023-04-14 14:22:28 -0400

    Establish MongoDB connection and ensure proper resource management in wanotifdb functions

--- WarNotifications/warnotif_database.py
+++ WarNotifications/warnotif_database.py
@@ -3,7 +3,7 @@ from os import environ
 from pymongo import MongoClient
 
 database_uri = environ.get('MONGO_URI')
-client = MongoClient(database_uri)['Wynncord']
+
 
 
 def check_tracking(guild_name: str):
@@ -12,26 +12,34 @@ def check_tracking(guild_name: str):
     :param guild_name:
     :return: True if its found.
     """
-    collection = client['guild_tracking']
+    client = MongoClient(database_uri)
+    collection = client['Wynncord']['guild_tracking']
     search = collection.find_one({'name': guild_name})
 
     if search:
+        client.close()
         return True
+    client.close()
     return False
 
 
 def create_tracking(guild_name: str, channel_id: int):
-    collection = client['guild_tracking']
+    client = MongoClient(database_uri)
+    collection = client['Wynncord']['guild_tracking']
     collection.insert_one({
         'name': guild_name,
         'channels': [channel_id]
     })
+    client.close()
 
 
 def get_channels(guild_name: str):
-    collection = client['guild_tracking']
+    client = MongoClient(database_uri)
+    collection = client['Wynncord']['guild_tracking']
     search = collection.find_one({'name': guild_name})
-    return search['channels']
+    result = search['channels']
+    client.close()
+    return result
 
 
 def add_tracking(guild_name: str, channel_id: int):
@@ -46,25 +54,31 @@ def add_tracking(guild_name: str, channel_id: int):
         create_tracking(guild_name, channel_id)
         return True
 
-    collection = client['guild_tracking']
+    client = MongoClient(database_uri)
+    collection = client['Wynncord']['guild_tracking']
     search = collection.find({'name': guild_name, 'channels': {"$elemMatch": {"$eq": channel_id}}})
 
     if len(list(search.clone())) != 0:
+        client.close()
         return False
 
     collection.update_one(
         {'name': guild_name},
         {'$push': {'channels': channel_id}}
     )
+    client.close()
     return True
 
 
 def rm_tracking(guild_name: str, channel_id: int):
-    collection = client['guild_tracking']
+    client = MongoClient(database_uri)
+    collection = client['Wynncord']['guild_tracking']
     search = collection.find_one({'name': guild_name, 'channels': {"$elemMatch": {"$eq": channel_id}}})
     if not search:
+        client.close()
         return False
 
     collection.update_one({'name': guild_name},
                           {'$pull': {'channels': channel_id}})
+    client.close()
     return True
